fe7c6d4294389a39c4933c79aba9496e0cb826b7
nathan <nathan.genillon42@gmail.com>
2025-12-01 09:47:04 +0100

    v4 avec changement langue

--- src/Controller/GenillonNathanController.php
+++ src/Controller/GenillonNathanController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class GenillonNathanController extends AbstractController
 {
@@ -16,14 +16,45 @@ final class GenillonNathanController extends AbstractController
         ]);
     }
 
-    #[Route("/cv", name: "app_cv")]
+    #[Route('/cv', name: 'app_cv')]
     public function cv(): Response
     {
-        return $this->render("genillon_nathan/cv.html.twig",);
+        return $this->render('genillon_nathan/cv.html.twig');
     }
-    #[Route("/portfolio", name: "app_portfolio")]
+
+    #[Route('/portfolio', name: 'app_portfolio')]
     public function portfolio(): Response
     {
-        return $this->render("genillon_nathan/portfolio.html.twig",);
+        return $this->render('genillon_nathan/portfolio.html.twig');
+    }
+
+    #[Route('/En_savoir_plus', name: 'app_En_savoir_plus')]
+    public function En_savoir_plus(): Response
+    {
+        return $this->render('genillon_nathan/En_savoir_plus.html.twig');
+    }
+    #[Route('/en/', name: 'app_genillon_nathan_en')]
+    public function indexeEn(): Response
+    {
+        return $this->render('genillon_nathan/EN_index.html.twig', [
+            'controller_name' => 'GenillonNathanController',
+        ]);
+    }
+    #[Route('/en/cv', name: 'app_cv_en')]
+    public function cvEn(): Response
+    {
+        return $this->render('genillon_nathan/EN_cv.html.twig');
+    }
+
+    #[Route('/en/portfolio', name: 'app_portfolio_en')]
+    public function portfolioEn(): Response
+    {
+        return $this->render('genillon_nathan/EN_portfolio.html.twig');
+    }
+
+    #[Route('/en/En_savoir_plus', name: 'app_En_savoir_plus_en')]
+    public function En_savoir_plusEn(): Response
+    {
+        return $this->render('genillon_nathan/EN_En_savoir_plus.html.twig');
     }
 }
