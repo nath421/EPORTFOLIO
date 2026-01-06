353619cb4bd788fa55b634576897d9fe6ed6dd35
nathan <nathan.genillon42@gmail.com>
2026-01-06 10:21:51 +0100

    v5 fini

--- src/Controller/GenillonNathanController.php
+++ src/Controller/GenillonNathanController.php
@@ -58,4 +58,35 @@ final class GenillonNathanController extends AbstractController
         return $this->render('genillon_nathan/EN_En_savoir_plus.html.twig');
     }
 
+    #[Route('/ac1101', name: 'app_ac1101')]
+    public function ac1101(): Response
+    {
+        return $this->render('genillon_nathan/ac1101.html.twig');
+    }
+    #[Route('/en/ac1101', name: 'app_En_ac1101')]
+    public function ac1101En(): Response
+    {
+        return $this->render('genillon_nathan/EN_ac1101.html.twig');
+    }
+    #[Route('/ac1102', name: 'app_ac1102')]
+    public function ac1102(): Response
+    {
+        return $this->render('genillon_nathan/ac1102.html.twig');
+    }
+    #[Route('/en/ac1102', name: 'app_En_ac1102')]
+    public function ac1102En(): Response
+    {
+        return $this->render('genillon_nathan/EN_ac1102.html.twig');
+    }
+    #[Route('ac1103', name: 'app_ac1103')]
+    public function ac1103(): Response
+    {
+        return $this->render('genillon_nathan/ac1103.html.twig');
+    }
+    #[Route('/en/ac1103', name: 'app_En_ac1103')]
+    public function ac1103En(): Response
+    {
+        return $this->render('genillon_nathan/EN_ac1103.html.twig');
+    }
+
 }
